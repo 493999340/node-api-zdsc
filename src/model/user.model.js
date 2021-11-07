210f5c3626d064f33e8d9334698fca4b39ab55f5
mina <493999340@qq.com>
2021-11-07 21:40:08 +0800

    拆分中间件1

---
+++ src/model/user.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+
+const seq = require("../db/seq");
+// 创建模型（zd_users）
+const User = seq.define("zd_user", {
+  // id会被自动创建
+  user_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    comment: "用户名，唯一",
+  },
+  password: {
+    type: DataTypes.CHAR(64),
+    allowNull: false,
+    comment: "密码",
+  },
+  is_admin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
+    comment: "是否管理员,0不是（默认），1是",
+  },
+});
+// 强制同步数据库（创建数据表）
+// User.sync({ force: true });
+module.exports = User;
