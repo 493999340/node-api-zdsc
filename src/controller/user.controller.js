78670f00cef48fe5063e1ff1abb58556b0feac6d
mina <493999340@qq.com>
2021-11-20 17:57:15 +0800

    引入jwt

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,7 @@
-const { createUser } = require("../service/user.service");
+const jwt = require("jsonwebtoken");
+const { createUser, getUserInfo } = require("../service/user.service");
 const { userRegisterError } = require("../constant/err.type");
-
+const { JWT_SECRET } = require("../config/config.default");
 class UserController {
   async register(ctx, next) {
     // 1. 获取数据
@@ -28,7 +29,21 @@ class UserController {
   }
 
   async login(ctx, next) {
-    ctx.body = "登录成功";
+    const { user_name } = ctx.request.body;
+    ctx.body = "欢迎回来" + user_name;
+    try {
+      // 从返回结果中剔除password属性，剩下的属性放到res对象里面
+      const { password, ...res } = await getUserInfo({ user_name });
+      ctx.body = {
+        code: 0,
+        message: "用户登陆成功",
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      };
+    } catch (error) {
+      console.error("用户登录失败", error);
+    }
   }
 }
 
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -5,6 +5,9 @@ const {
   userFormatError,
   userAlreadyExited,
   userRegisterError,
+  userDoesNotExist,
+  invalidPassword,
+  userLoginError,
 } = require("../constant/err.type");
 
 const userValidator = async (ctx, next) => {
@@ -40,4 +43,30 @@ const cryptPassword = async (ctx, next) => {
   ctx.request.body.password = hash;
   await next();
 };
-module.exports = { userValidator, verifyUser, cryptPassword };
+const verifyLogin = async (ctx, next) => {
+  // 1. 判断用户是否存在(不存在:报错)
+  const { user_name, password } = ctx.request.body;
+
+  try {
+    const res = await getUserInfo({ user_name });
+
+    if (!res) {
+      console.error("用户名不存在", { user_name });
+      ctx.app.emit("error", userDoesNotExist, ctx);
+      return;
+    }
+
+    // 2. 密码是否匹配(不匹配: 报错)
+    if (!bcrypt.compareSync(password, res.password)) {
+      console.log(res.password);
+      ctx.app.emit("error", invalidPassword, ctx);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    return ctx.app.emit("error", userLoginError, ctx);
+  }
+
+  await next();
+};
+module.exports = { userValidator, verifyUser, cryptPassword, verifyLogin };
